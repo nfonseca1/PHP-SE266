e5a9d40d4e230aa054ffe3ff88206caf2466a007
Fonseca <nrfonseca@labs.neit.edu>
2018-03-28 10:35:19 -0400

    Finished Table

--- mult.php
+++ mult.php
@@ -4,8 +4,22 @@ $table = "<table>\n";
 for ($rows = 1; $rows <= $size; $rows++)
 {
     $table .= "\t<tr>";
+    for ($cols = 1; $cols<=$size; $cols++)
+    {
+        $table.="<td>" . $rows * $cols . "</td>";
+    }
     $table .= "</tr>\n";
 }
 $table .= "</table>\n";
 echo $table;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Multiplication Table</title>
+</head>
+<body>
+<?php echo $table; ?>
+</body>
+</html>
